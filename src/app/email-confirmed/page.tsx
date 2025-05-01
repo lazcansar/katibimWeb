19629ae1c56097ce1af4f64e8aca2dff2e323e62
lazcansar <56204736+lazcansar@users.noreply.github.com>
2025-05-01 16:57:26 +0300

    New Page Created

---
+++ src/app/email-confirmed/page.tsx
@@ -0,0 +1,14 @@
+"use client"
+import { useRouter } from "next/navigation";
+
+const EmailConfirmedPage = () => {
+    const router = useRouter();
+
+    return (
+        <div className="flex flex-col items-center justify-center py-16 rounded-lg px-4 lg:px-0 h-screen">
+
+        </div>
+    );
+};
+
+export default EmailConfirmedPage;
